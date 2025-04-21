a0dd3f43aa4d85ea14a4ca5c5e3b96f1d4f4593f
Jungmaba <51909491+Jungmaba@users.noreply.github.com>
2025-04-21 17:50:11 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+const v = Number(input[2]);
+
+let count = 0;
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === v) {
+    count++;
+  }
+}
+console.log(count);
