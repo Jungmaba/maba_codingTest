e7c153eb3c24d124366eb607fae6b96594132c8b
Jungmaba <51909491+Jungmaba@users.noreply.github.com>
2025-04-22 16:57:08 +0900

    Update 행렬 덧셈.js

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.js
@@ -16,3 +16,11 @@ for (let i = n + 1; i <= 2 * n; i++) {
 
 const result = a.map((row, i) => row.map((value, j) => value + b[i][j]));
 result.forEach((row) => console.log(row.join(' ')));
+
+
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// const [n, m] = input[0].split(' ').map(Number);
+// const a = input.slice(1, 1 + n).map((line) => line.split(' ').map(Number));
+// const b = input.slice(1 + n, 1 + 2 * n).map((line) => line.split(' ').map(Number));
+// const result = a.map((row, i) => row.map((value, j) => value + b[i][j]));
+// result.forEach((row) => console.log(row.join(' ')));
