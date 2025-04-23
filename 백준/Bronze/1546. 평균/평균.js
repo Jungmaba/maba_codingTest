149e0e110e45727b918686bcbc4d2a204a6b4dd5
Jungmaba <51909491+Jungmaba@users.noreply.github.com>
2025-04-23 17:23:38 +0900

    Update 평균.js

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -6,3 +6,40 @@ const maxValue = Math.max.apply(null, b);
 const newB = b.map((value) => (value / maxValue) * 100);
 const reuslt = newB.reduce((a, b) => a + b) / a;
 console.log(reuslt.toFixed(2));
+
+
+// 메서드 라이브러리 사용하지 않고 코드 짜보기
+// const fs = ['3', '80 40 80'];
+// const a = Number(fs[0]);
+// const b = fs[1].split(' ').map(Number);
+
+// const max = (arr) => {
+//   let tmp = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (tmp > arr[i]) {
+//       continue;
+//     } else tmp = arr[i];
+//   }
+//   return tmp;
+// };
+
+// const maxValue = max(b);
+
+// const newScore = (arr2, max = maxValue) => {
+//   let score = [];
+//   for (let i = 0; i < arr2.length; i++) {
+//     score[i] = (arr2[i] / maxValue) * 100;
+//   }
+//   return score;
+// };
+
+// const newAverage = (arr3) => {
+//   let avg = 0;
+//   for (let i = 0; i < arr3.length; i++) {
+//     avg += arr3[i];
+//   }
+//   return avg / arr3.length;
+// };
+// const newValue = newScore(b);
+// const newAverageValue = newAverage(newValue);
+// console.log(newAverageValue.toFixed(2));
