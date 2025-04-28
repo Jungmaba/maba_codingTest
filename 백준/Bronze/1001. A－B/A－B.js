7baa7d3d8dd1c846d51320898445c4f646a70066
Jungmaba <51909491+Jungmaba@users.noreply.github.com>
2025-04-28 15:32:26 +0900

    Update A－B.js

--- 백준/Bronze/1001. A－B/A－B.js
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -1,3 +1,3 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-const n = [x ,y] = input[0].split(" ").map(Number);
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n'); //입력 받기
+const n = [x ,y] = input[0].split(" ").map(Number); //문자열로 받은 input 을 " " 통해 나누고 map() 함수를 통해 문자열 -> 숫자타입으로 바꿔주고 구조 분해로 각 나눠진 값 x, y에 집어넣기
 console.log(x - y);
